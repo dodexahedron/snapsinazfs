2e91a06c9c450cc146eadac761aa411895522ecd
Brandon Thetford <github@snapsinazfs.com>
2026-06-16 20:52:40 -0700

    Make these private and remove unnecessary inline hint

--- Applications/SnapsInAZfs/Program.cs
+++ Applications/SnapsInAZfs/Program.cs
@@ -129,7 +129,7 @@ internal class Program
         }
     }
 
-    internal static bool LoadConfigurationFromConfigurationFiles( [NotNullWhen( true )] ref SnapsInAZfsSettings? settings, [NotNullWhen( true )] out IConfigurationRoot? rootConfiguration, in CommandLineArguments args )
+    private static bool LoadConfigurationFromConfigurationFiles( [NotNullWhen( true )] ref SnapsInAZfsSettings? settings, [NotNullWhen( true )] out IConfigurationRoot? rootConfiguration, in CommandLineArguments args )
     {
         // Configuration is built in the following order from various sources.
         // Configurations from all sources are merged, and the final configuration that will be used is the result of the merged configurations.
@@ -191,8 +191,7 @@ internal class Program
         return true;
     }
 
-    [MethodImpl( MethodImplOptions.AggressiveInlining )]
-    internal static void SetCommandLineLoggingOverride( CommandLineArguments args )
+    private static void SetCommandLineLoggingOverride( CommandLineArguments args )
     {
         if ( args.Debug )
         {
