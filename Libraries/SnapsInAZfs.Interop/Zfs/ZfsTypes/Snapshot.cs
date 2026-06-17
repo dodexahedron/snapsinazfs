614bb38e2d28d90b22563459423c60b9424767b4
Brandon Thetford <github@snapsinazfs.com>
2026-06-16 20:54:37 -0700

    Use ordinal comparison

--- Libraries/SnapsInAZfs.Interop/Zfs/ZfsTypes/Snapshot.cs
+++ Libraries/SnapsInAZfs.Interop/Zfs/ZfsTypes/Snapshot.cs
@@ -211,7 +211,7 @@ public sealed partial record Snapshot : ZfsRecord, IComparable<Snapshot>, IEqual
         }
 
         // If timestamps are different, sort on period, by its SnapshotPeriodKind equivalent
-        return !Period.Value.Equals ( other.Period.Value )
+        return !Period.Value.Equals(other.Period.Value, StringComparison.Ordinal)
                    ? Period.Value.ToSnapshotPeriodKind ( ).CompareTo ( other.Period.Value.ToSnapshotPeriodKind ( ) )
                    : string.Compare ( Name, other.Name, StringComparison.Ordinal );
     }
